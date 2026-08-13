99785b530ae8b473ff39eee549e7c393d333df90
Yomara <yomara024@outlook.com>
2026-08-13 10:08:51 -0400

    merged

--- src/components/Checklist.jsx
+++ src/components/Checklist.jsx
@@ -1,29 +1,66 @@
-function Checklist({ trip }) {
-  console.log(trip.Checklists);
+import { useState, useEffect } from "react";
 
-async function check(params) {
-  
-  
-}
 
+function Checklist({ trip, setTrip }) {
+
+  // const [checkBox, setCheckBox] = useState(false);
+  // const [checked, setChecked]= useState(false)
+  // console.log(trip.Checklists);
+
+  async function checkEdit(e, item) {
+    e.preventDefault()
+    const response = await fetch(
+      `http://localhost:8080/trips/${trip.id}/${item.id}/checklist/edit`,
+      {
+        method: "PATCH",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({completed: !item.completed})
+      },
+    );
+
+    if (!response.ok){
+      alert("Faled to save checklist.")
+    return
+    }
+    const updatedchecked= await response.json()
+    // console.log(updatedchecked)
+    const newTrip = {...trip}
+
+    newTrip.Checklists = newTrip.Checklists.map((item) => {
+      if(item.id === updatedchecked.id){
+        return updatedchecked
+      } else return item
+    })
+
+    // console.log(newTrip)
+    setTrip(newTrip)
+
+  }
+
+  const markCheck = (item,index,e) => {
+    checkEdit(e,trip.Checklists[index])
+  };
 
   return (
     <>
-
       <form>
-      <fieldset>
-        <legend>Checklist</legend>
-        {trip.Checklists.map((list, index) => (
-          <label key={list.text} value={list.text}>
-            <ul>
-              <li>
-                <input type="checkbox" id="checklist"  />{" "}
-                {list.text}
-              </li>
-            </ul>
-          </label>
-        ))}
-      </fieldset>
+        <fieldset>
+          <legend>Checklist</legend>
+          {trip.Checklists.map((list, index) => (
+            <label key={list.id} value={list.text}>
+              <ul>
+                <li>
+                  <input checked={list.completed} onChange={(e)=> markCheck(list,index,e)} type="checkbox" id="checklist" /> {list.text}
+                </li>
+              </ul>
+            </label>
+          ))}
+        </fieldset>
+        {/* <button
+        onClick={checkEdit}>Save</button> */}
       </form>
     </>
   );
